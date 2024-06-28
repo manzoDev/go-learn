612eb3261325634ce1d6aa5bfef2cb78a0f4c00e
manzoDev <manzoroger08@gmail.com>
2024-06-28 09:41:39 -0400

    change func Addpoint adding * pointer

--- pointer_receivers/main.go
+++ pointer_receivers/main.go
@@ -12,13 +12,23 @@ func main() {
 		Points:0,
 	}
 
+	//direction player
+	fmt.Printf("Player address: %p\n", &player1)
+
 	fmt.Println("Player Points:", player1.Points)
 
 	// first game add 2 points
+	player1.AddPoints(2);
+
+	// can try points player 1
+	fmt.Println("Player points:", player1.Points)
 
 }
 
-func (r Player) AddPoints(points int) {
+func (r *Player) AddPoints(points int) {
 	
+	// verify receiver direction
+	fmt.Printf("Player address: %p\n", r)
+
 	r.Points += points
 } 
