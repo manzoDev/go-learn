daeaee307b2fd62330a961e93fad76d2689e4111
manzoDev <manzoroger08@gmail.com>
2024-06-29 13:25:05 -0400

    add interface

--- interface/main.go
+++ interface/main.go
@@ -15,6 +15,19 @@ type Circle struct {
 	Radius float64
 }
 
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return 3.14 * r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return  2 * (r.Width + r.Height) 
+}
+
 func (c Circle) Area() float64 {
 	return 3.14 * c.Radius * c.Radius
 }
