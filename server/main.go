4676f80971687dcd3199bcc2249d89476da0f829
manzoDev <manzoroger08@gmail.com>
2024-06-30 13:24:38 -0400

    use echo for listen server

---
+++ server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	
+	e := echo.New()
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "¡PONG!")
+	}) 
+
+	e.Logger.Fatal(e.Start(":9999"))
+}
