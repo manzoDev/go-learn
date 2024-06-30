02e8e98355da9663b540ff8e9fc7ac98c21d4ce3
manzoDev <manzoroger08@gmail.com>
2024-06-30 13:28:49 -0400

    adding route for parameter

--- server/main.go
+++ server/main.go
@@ -6,13 +6,20 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
 func main() {
 	
 	e := echo.New()
 
 	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, "¡PONG!")
-	}) 
+	})
+
+	e.GET("user/:id", getUser)
 
 	e.Logger.Fatal(e.Start(":9999"))
 }
